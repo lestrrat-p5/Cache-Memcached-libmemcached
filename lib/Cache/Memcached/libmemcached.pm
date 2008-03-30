b05cbbfee9942e9e2db2b7847abc9e8019c971b8
Daisuke Maki <lestrrat+github@gmail.com>
2008-03-30 14:57:41 +0000

    lang/perl/Cache-Memcached-libmemcached; version++
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Cache-Memcached-libmemcached/trunk@8530 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Cache/Memcached/libmemcached.pm
+++ lib/Cache/Memcached/libmemcached.pm
@@ -10,7 +10,7 @@ use base qw(Memcached::libmemcached);
 use Carp qw(croak);
 use Storable ();
 
-our $VERSION = '0.02001';
+our $VERSION = '0.02002';
 
 use constant HAVE_ZLIB    => eval { require Compress::Zlib } && !$@;
 use constant F_STORABLE   => 1;
